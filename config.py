4fbb31a9cc291118bd665080e2d42a7187380f54
Trinanjan Mitra <61656144+mtrino@users.noreply.github.com>
2026-07-29 12:52:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+import cohere
+from dotenv import load_dotenv
+from qdrant_client import QdrantClient
+from fastembed import SparseTextEmbedding
+
+load_dotenv()
+
+co = cohere.ClientV2(api_key=os.environ["COHERE_API_KEY"])
+qdrant = QdrantClient(url=os.environ["QDRANT_URL"], api_key=os.environ["QDRANT_API_KEY"])
+bm25 = SparseTextEmbedding("Qdrant/bm25")
+
+COLLECTION_NAME = "your_collection_name"
