83ac9650d9b669cc3e2a7e77d60ce9faed9eee28
Trinanjan Mitra <61656144+mtrino@users.noreply.github.com>
2026-07-29 12:50:10 +0530

    Create _rag_search.py

---
+++ tools/_rag_search.py
@@ -0,0 +1,36 @@
+from qdrant_client.models import SparseVector, FusionQuery, Prefetch
+
+from config import co, bm25, qdrant, COLLECTION_NAME
+
+
+def search(question: str, fetch: int = 20, k: int = 10):
+    """Searches the vector DB to generate relevant answers."""
+
+    dense = co.embed(
+        model="embed-v4.0",
+        input_type="search_query",
+        embedding_types=["float"],
+        texts=[question]
+    ).embeddings.float[0]
+
+    sv = next(iter(bm25.query_embed([question])))
+
+    sparse = SparseVector(indices=sv.indices.tolist(), values=sv.values.tolist())
+
+    candidates =  qdrant.query_points(
+        collection_name=COLLECTION_NAME,
+        prefetch=[
+            Prefetch(query=dense, using="dense", limit=fetch),
+            Prefetch(query=sparse, using="sparse", limit=fetch),
+        ],
+        query=FusionQuery(fusion="rrf"),
+        limit=k
+    ).points
+
+    if not candidates:
+        return {"result": []}
+
+    return {"result": [
+        {"text": r.payload["text"], "score": r.score}
+        for r in candidates
+    ]}
